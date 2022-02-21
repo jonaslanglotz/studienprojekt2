b685557e89d7a100ee969aef2999df57209cb611
Jonas Langlotz <me@jonas-langlotz.de>
2022-02-21 21:25:06 +0100

    Fixed Build Errors

--- Assets/TrailManagerScript.cs
+++ Assets/TrailManagerScript.cs
@@ -2,10 +2,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Cryptography;
-using Unity.CodeEditor;
-using Unity.Collections;
-using UnityEditor.SceneManagement;
 using UnityEngine;
 
 public class TrailManagerScript : MonoBehaviour
